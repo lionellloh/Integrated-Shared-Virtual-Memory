de989a14da0fa664445a7ef088352d1f259a0233
Lionell <lionell_loh@mymail.sutd.edu.sg>
2020-04-19 16:47:56 +0800

    general structs structure up

--- ivy.go
+++ ivy.go
@@ -1,1 +1,52 @@
-package pset3
+package main
+
+import "fmt"
+
+type AccessType int
+
+const (
+	ReadOnly AccessType = iota
+	WriteOnly
+	ReadWrite
+
+)
+
+func (a AccessType) String() string {
+	return [...]string{"ReadOnly", "WriteOnly", "ReadWrite"}[a]
+}
+
+
+type OwnershipDetails struct {
+	copySet map[string]bool
+	owner *Process
+
+}
+
+type CentralManager struct {
+
+}
+
+
+type Process struct {
+	id string
+	localMemory map[string] AccessTypePage
+
+}
+
+//func (p *Process) request
+
+type AccessTypePage struct {
+	accessType AccessType
+	page *Page
+
+}
+
+
+type Page struct {
+	id string
+	content string
+}
+
+func main(){
+	fmt.Println(ReadOnly)
+}
